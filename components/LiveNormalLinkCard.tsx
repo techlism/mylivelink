ab7457160e55583496eeb24ccf106d7e3c6e181e
techlism <105585386+techlism@users.noreply.github.com>
2024-02-25 20:23:06 +0530

    version 0.9.0 -- changelogs in readme

--- components/LiveNormalLinkCard.tsx
+++ components/LiveNormalLinkCard.tsx
@@ -43,30 +43,25 @@ export default function LiveNormalLinkCard({ link }: { link: LinkSchema }) {
     setTimeout(() => setIsCopied(false), 2000);
   };
   console.log(bgColor, bgColor2);
-  return (
-    <Link href={link.url} target="blank">
+  return (    
     <Card className = {`mt-4 w-full grid grid-cols-1 items-center sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 hover:opacity-90`} style={{background:`linear-gradient(${bgColor}, ${bgColor2})`}} >
-            <CardContent className='m-4 p-0 pt-0.5 h-[96px] w-[145px] grid grid-cols-1 items-center sticky top-0'>
-                {link.thumbnailUrl && link.thumbnailUrl !== '' ? 
-                    <img src={link.thumbnailUrl} alt={link.title} className="rounded-lg max-h-[95px] max-w-[143px]"/> :
-                    link.faviconURL && link.faviconURL !== '' &&
-                    <img src={link.faviconURL} alt={link.title} className="rounded-lg max-h-[50px] max-w-[50px]"/>
-                }
-            </CardContent>
-            <CardHeader>
-                <CardTitle className='font-medium'>{link.title}</CardTitle>                
-            </CardHeader>
+            <Link href={link.url} target="blank">
+              <CardContent className='m-4 p-0 pt-0.5 h-[96px] w-[145px] grid grid-cols-1 items-center sticky top-0'>
+                  {link.thumbnailUrl && link.thumbnailUrl !== '' ? 
+                      <img src={link.thumbnailUrl} alt={link.title} className="rounded-lg max-h-[95px] max-w-[143px]"/> :
+                      link.faviconURL && link.faviconURL !== '' &&
+                      <img src={link.faviconURL} alt={link.title} className="rounded-lg max-h-[50px] max-w-[50px]"/>
+                  }
+              </CardContent>
+              <CardHeader>
+                  <CardTitle className='font-medium'>{link.title}</CardTitle>                
+              </CardHeader>
+            </Link>            
           <CardFooter className='p-3 m-0 mr-4 grid grid-cols-2 gap-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 items-center'>
-            {/* <Button className="hover:scale-105">
-              <Link href={link.url} target="blank">
-                <ArrowUpRightSquare />
-              </Link>
-            </Button> */}
             <Button onClick={handleCopy} className="hover:scale-105">
               {isCopied ? <CopyCheck /> : <CopyIcon />}
             </Button>
           </CardFooter>
     </Card>
-    </Link>
   );
 }
